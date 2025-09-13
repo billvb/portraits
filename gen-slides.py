9a895d2e52b5500ad2c1b7af6b9333039fcd7837
billvb <vanbesien@gmail.com>
2025-09-12 23:15:02 -0400

    Almost finished, in good shape, just need JPGs with less artifacts

--- gen-slides.py
+++ gen-slides.py
@@ -1,6 +1,6 @@
 import os
 
-imgs = [f for f in os.listdir('sample') if 'WEB' in f]
+imgs = [f for f in os.listdir('gallery') if '' in f]
 
 for img in imgs:
-    print(f'<div class="swiper-slide"><img src="sample/{img}" alt="{img}"></div>')
+    print(f'<div class="swiper-slide"><img src="/gallery/{img}" alt="{img}"></div>')
